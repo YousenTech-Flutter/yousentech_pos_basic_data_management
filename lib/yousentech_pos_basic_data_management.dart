526af8fb044b81c158b2ee191ba2f6a25dfa4f72
mohmmedsaleh <mohmmed123232@gmail.com>
2025-01-28 10:19:54 +0300

    .

--- lib/yousentech_pos_basic_data_management.dart
+++ lib/yousentech_pos_basic_data_management.dart
@@ -4,3 +4,14 @@
 //   int addOne(int value) => value + 1;
 // }
 library yousentech_pos_basic_data_management;
+
+export 'src/account_journal/presentation/account_journal_list_screen.dart';
+export 'src/account_tax/presentation/account_tax_list_screen.dart';
+export 'src/customer/presentation/views/customers_list_screen.dart';
+export 'src/customer/presentation/views/create_customer.dart';
+export 'src/pos_categories/presentation/views/add_edit_pos_category_screen.dart';
+export 'src/pos_categories/presentation/views/pos_categories_list_screen.dart';
+export 'src/product_unit/presentation/add_edit_pos_product_unit_screen.dart';
+export 'src/product_unit/presentation/pos_product_unit_list_screen.dart';
+export 'src/products/presentation/views/add_edit_product_screen.dart';
+export 'src/products/presentation/views/product_list_screen.dart';
