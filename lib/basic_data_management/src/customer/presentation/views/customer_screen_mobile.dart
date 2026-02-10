36f7a991197502b159c03e334f2fcba503451e4c
eemzain <hudazain55@gmail.com>
2026-02-10 10:50:06 +0300

    .

--- lib/basic_data_management/src/customer/presentation/views/customer_screen_mobile.dart
+++ lib/basic_data_management/src/customer/presentation/views/customer_screen_mobile.dart
@@ -725,7 +725,7 @@ class _CustomerScreenMobileState extends State<CustomerScreenMobile> {
                                                   image: item.image == null ||
                                                           isSvg(item.image
                                                               .toString())
-                                                      ? AssetImage(AppImages.partner,
+                                                      ? AssetImage(AppImages.productEmptySvg,
                                                           package:
                                                               'shared_widgets',
                                                         )
